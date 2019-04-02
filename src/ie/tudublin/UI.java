97db0e917bc02d7e80a0dc55e791ab45d8cb05fe
sahil sahil <d17124269@mydit.ie>
2019-04-03 00:20:28 +0100

    created an UI page where i am gonna do main interface stuff

---
+++ src/ie/tudublin/UI.java
@@ -0,0 +1,16 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class UI extends PApplet
+{
+    public void settings()
+    {
+        size(1000,800);
+    }
+
+    public void setup()
+    {
+        
+    }
+}
