d9fc1d7d9942abd442f9ab7cb87e409949851190
sahil sahil <d17124269@mydit.ie>
2019-04-02 23:57:56 +0100

    first commit

---
+++ src/ie/tudublin/SpaceObject.java
@@ -0,0 +1,19 @@
+package ie.tudublin;
+
+
+public abstract class SpaceObject 
+{
+    public SpaceObject()
+    {
+        
+    }
+
+    public void render()
+    {
+
+    }
+    public void update()
+    {
+        
+    }
+}
