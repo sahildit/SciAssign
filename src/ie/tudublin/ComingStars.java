b4461d102d46ed05f0d66ea10ae012f49f7a4988
sahil sahil <d17124269@mydit.ie>
2019-04-03 00:39:45 +0100

    made an page called ComingStars where i am gonna put stars as blinking kind of way

---
+++ src/ie/tudublin/ComingStars.java
@@ -0,0 +1,13 @@
+package ie.tudublin;
+
+import prcessing.core.PApplet;
+
+public class ComingStars extends SpaceObject
+{
+    UI ui;
+
+    public ComingStars(UI ui)
+    {
+        this.ui = ui;
+    }
+}
