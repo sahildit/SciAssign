9f6196f9e8134e6139873c33116822711bcc7b48
sahil sahil <d17124269@mydit.ie>
2019-04-03 00:09:04 +0100

    Made an main class

---
+++ src/ie/tudublin/Main.java
@@ -0,0 +1,19 @@
+package ie.tudublin;
+
+public class Main
+{
+    public void UI()
+    {
+        String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch(a, new UI());
+
+
+    }
+
+    public static void main(String[] args)
+    {
+
+        Main main = new Main();
+        main.UI();
+    }
+}
