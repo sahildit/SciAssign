4777bb78bed69508e7078285e61f6175c5ec2bf9
sahil sahil <d17124269@mydit.ie>
2019-04-03 22:41:21 +0100

    finally got my radar axis where i want them

--- src/ie/tudublin/Radar.java
+++ src/ie/tudublin/Radar.java
@@ -5,16 +5,20 @@ import processing.core.PApplet;
 public class Radar
 {
     PApplet ui;
+    float r;
 
     public Radar(PApplet ui)
     {
         this.ui = ui;
-        float r;
+        this.r = r;
+        
     }
 
     public void render()
     {
-        ui.ellipse(50,300,200,300);
-        ui.ellipse(50,600,150,159);
+        ui.ellipse(100,600,200,200);
+        ui.ellipse(100,600,150,150);
+        ui.ellipse(100,600,150,150);
+        ui.ellipse(100,600,50,50);
     }
 }
