9cd3e69c3ea1a92b338aa7919dce73f09d2d8ca5
sahil sahil <d17124269@mydit.ie>
2019-04-03 02:39:09 +0100

    just making code little bit more smooth and till this its working the way i want it to work so i am happy ...yepe

--- src/ie/tudublin/Main.java
+++ src/ie/tudublin/Main.java
@@ -3,17 +3,17 @@ package ie.tudublin;
 public class Main
 {
     public void UI()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch(a, new UI());
-
-
-    }
-
-    public static void main(String[] args)
-    {
-
-        Main main = new Main();
-        main.UI();
-    }
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new UI());
+		
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+		main.UI();	
+	}
 }
+
--- src/ie/tudublin/UI.java
+++ src/ie/tudublin/UI.java
@@ -1,7 +1,9 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import java.util.ArrayList;
 import processing.core.PImage;
+
 
 
 public class UI extends PApplet
@@ -10,68 +12,83 @@ public class UI extends PApplet
     PImage img;
     BarLoad loading;
     ComingStars stars;
-
+    ArrayList<SpaceObject> items = new ArrayList<SpaceObject>();
+    
+    
     public void settings()
     {
         size(1000,800);
     }
-
     public void setup()
     {
         stars = new ComingStars(this);
         items.add(stars);
         loading = new BarLoad(this);
         
+        
+        
     }
-
     public void draw()
     {
-        background(255);
         
+        
+        background(255);
         noFill();
         int k = loadbar(counter);
         counter++;
-
+        
         if(k>=200)
         {
+            clearScreen();
             for(int i = items.size()-1; i>=0;i--)
-            {
-                SpaceObject j =items.get(i);
-                j.render();
-            }
+                {
+                    SpaceObject j = items.get(i);
+                    j.render();
+            
+                }
         }
-
+        
+       
+        
+        
     }
-
+   
     public int loadbar(int inc)
     {
-        img = loadImage("planetImageResize.jpg");
-        rect(50,450,200,50);
-        image(img,0,0);        
-
-        // ask bryan in class 
         
-        text("PROJECT : SECRET DOJO",400,200);
-        stroke(0,255,255);
+    img = loadImage("planetImageResize.jpg");
+    
+    
+    rect(50,450,200,50);
+    image(img,0,0);
+    
 
+    fill(255);
+    //ui.textAlign(CENTER);
+    //FONT font1 = new Font("SansSerif", Font.BOLD,20);
+    text("PROJECT : SECRET DOJO",400,200);
+    stroke(0,255,255);
+   
+   
+    
         if(inc<200)
-        {
-            fill(0);
-            rect(50,450,inc,50);
-            stroke(200);
-        }
-        if(inc>=200)
-        {
-            fill(0);
-            rect(50,450,200,50);
-            return inc;
-        }
-        return inc;
-        
-        
+    {  
+        fill(0);
+        rect(50,450,inc,50);
+        stroke(200);
     }
+    if(inc>=200)
+    {
+        fill(0);
+        rect(50,450,200,50);
+        return inc;
+    }
+    return inc;
+    }
+    
     public void clearScreen()
     {
         background(0);
     }
+
 }
