8f343e7d30a625f6da49a338aaee3485c2ef8969
sahil sahil <d17124269@mydit.ie>
2019-04-03 02:48:55 +0100

    fix the error

--- src/ie/tudublin/BarLoad.java
+++ src/ie/tudublin/BarLoad.java
@@ -6,46 +6,48 @@ public class BarLoad extends SpaceObject
     UI ui;
     int counter;
     private PImage img;
+    
+   public BarLoad(UI ui)
+   {
+       this.ui = ui ;
 
-    public BarLoad(UI ui)
-    {
-        this.ui = ui;
-    }
+   }
+   public void render()
+   {
+    img = ui.loadImage("planetImageResize.jpg");
+    counter++;
+    
+    ui.rect(50,450,200,50);
+    ui.image(img,0,0);
+    update(counter);
 
-    public void render()
-    {
-        img = ui.loadImage("planetImageResize.jpg");
-        counter++;
-
-        ui.rect(50,450,200,50);
-        ui.image(img,0,0);
-        ui.update(counter);
-        
-
-        ui.fill(255);
-        ui.text("PROJECT : SECRET DOJO",400,200);
-        ui.stroke(0,255,255);
-    }
-
-    public void update(int inc)
-    {
+    ui.fill(255);
+    //ui.textAlign(CENTER);
+    //FONT font1 = new Font("SansSerif", Font.BOLD,20);
+    ui.text("PROJECT : SECRET DOJO",400,200);
+    ui.stroke(0,255,255);
+   }
+   public void update(int inc)
+   {
         if(inc<200)
-        {
-            ui.fill(0);
-            ui.rect(50,450,inc,50);
-            ui.stroke(200);
-        }
-        
-        if(inc>=200)
-        {
-            ui.fill(0);
-            ui.rect(50,450,200,50);
-
-        }
-
+    {  
+        ui.fill(0);
+        ui.rect(50,450,inc,50);
+        ui.stroke(200);
     }
+    if(inc>=200)
+    {
+        ui.fill(0);
+        ui.rect(50,450,200,50);
+        
+    }
+
+    
+   }
     public void remove()
     {
         ui.background(0);
-    }
+    } 
+   
+ 
 }
--- src/ie/tudublin/ComingStars.java
+++ src/ie/tudublin/ComingStars.java
@@ -1,29 +1,28 @@
 package ie.tudublin;
 
-import prcessing.core.PApplet;
-
+import processing.core.PApplet;
 public class ComingStars extends SpaceObject
 {
     UI ui;
-
-    public ComingStars(UI ui)
+    
+    public  ComingStars(UI ui)
     {
         this.ui = ui;
     }
-    
     public void render()
     {
         float r = ui.random(0,1000);
         float w = ui.random(0,799);
+        // it will increase the speed of star occuring on screen
         ui.fill(0,5);
         ui.rect(0,0,1000,799);
         ui.fill(255);
         ui.ellipse(r,w,3,3);
         ui.noStroke();
+        
     }
-
     public void update()
     {
-
+     
     }
 }
