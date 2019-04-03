2b1303849f1d99b9940f8d6b920ec0970fc4998e
sahil sahil <d17124269@mydit.ie>
2019-04-03 01:47:30 +0100

    update counter

--- src/ie/tudublin/BarLoad.java
+++ src/ie/tudublin/BarLoad.java
@@ -19,13 +19,16 @@ public class BarLoad extends SpaceObject
 
         ui.rect(50,450,200,50);
         ui.image(img,0,0);
+        ui.update(counter);
         
 
         ui.fill(255);
         ui.text("PROJECT : SECRET DOJO",400,200);
         ui.stroke(0,255,255);
-        
-        
+    }
 
+    public void update()
+    {
+        
     }
 }
