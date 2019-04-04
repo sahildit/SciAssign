709d5417bd8871b665d5e9aacab9a1693836477a
sahil sahil <d17124269@mydit.ie>
2019-04-04 10:26:21 +0100

    adjusting the vertices of ship

--- src/ie/tudublin/Sketch.java
+++ src/ie/tudublin/Sketch.java
@@ -15,27 +15,37 @@ public class Sketch
     }
     public void render()
     {
-        ui.beginShape();
-        ui.fill(0);
-        ui.strokeWeight(4);
-        ui.vertex(ui.width,(ui.height/2)-40);
-        ui.vertex(ui.width,(ui.height/2)+90);
-        ui.vertex(ui.width-140,(ui.height/2)-40);
-        ui.vertex(ui.width-140, (ui.height/2)+90);
-        ui.vertex(ui.width-340,(ui.height/2)+240);
-        ui.vertex(ui.width,ui.height);
-        ui.endShape(ui.CLOSE);
-    
-        ui.createShape();
-        ui.beginShape();
-        ui.fill(0);
-        ui.stroke(200);
-        ui.strokeWeight(4);
-        ui.vertex(ui.width-340,ui.height);
-        ui.vertex(ui.width-340,(ui.height/2)+240);
-        ui.vertex(290,(ui.height/2)+240);
-        ui.vertex(290,ui.height);
-        ui.endShape(ui.CLOSE);
 
+    //     ui.beginShape();
+    //     ui.fill(0);
+    //     ui.strokeWeight(4);
+    //     ui.vertex(ui.width,(ui.height/2)-40);
+    //     ui.vertex(ui.width,(ui.height/2)+90);
+    //     ui.vertex(ui.width-140,(ui.height/2)-40);
+    //     ui.vertex(ui.width-140, (ui.height/2)+90);
+    //     ui.vertex(ui.width-340,(ui.height/2)+240);
+    //     ui.vertex(ui.width,ui.height);
+    //     ui.endShape(ui.CLOSE);
+    
+    //     ui.createShape();
+    //     ui.beginShape();
+    //     ui.fill(0);
+    //     ui.stroke(200);
+    //     ui.strokeWeight(4);
+    //     ui.vertex(ui.width-340,ui.height);
+    //     ui.vertex(ui.width-340,(ui.height/2)+240);
+    //     ui.vertex(290,(ui.height/2)+240);
+    //     ui.vertex(290,ui.height);
+    //     ui.endShape(ui.CLOSE);
+
+            ui.beginShape();
+            ui.fill(255);
+            ui.strokeWeight(4);
+            ui.vertex(0,500);
+            ui.vertex(100,500);
+            ui.vertex(500,750);
+            ui.vertex(0,700);
+            ui.endShape(ui.CLOSE);
+        
     }
 }
